b7d313ae5bc32559c6d5d1e94842b0749e9e3992
rishika-07 <rishikakhajuria@outlook.com>
2023-06-19 23:32:07 +0530

    feat:bootstrap integration and data allocation via array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import {Navbar} from './components/navbar';
+import {Card} from './components/card';
 import './App.css';
 
+const photos = [
+  'https://picsum.photos/id/1001/200/200', 
+  'https://picsum.photos/id/1002/200/200', 
+  'https://picsum.photos/id/1003/200/200', 
+  'https://picsum.photos/id/1004/200/200', 
+  'https://picsum.photos/id/1005/200/200', 
+  'https://picsum.photos/id/1006/200/200',
+  'https://picsum.photos/id/1/200/300',
+  'https://picsum.photos/id/6/200/300',
+  'https://picsum.photos/id/11/200/300'
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="landing" style={{color:"blue"}}>
+    {/* fragments <> is used when we have to tags together so we need to put up a parent tag */}
+    <Navbar/>
+    <div className="headding text-center">
+    <h1>GALLERY</h1>
+     </div>
+     <div className="row">
+    {photos.map((photo)=><Card src={photo}/>)}
     </div>
+    </div>
+   </>
   );
 }
 
---
+++ src/components/card.js
@@ -0,0 +1,12 @@
+export function Card(props){
+    return(
+     <div className="col mb-5">
+  <div class="card" style={{width: "12rem", margin:"2rem", padding:"2px",height:"16rem"}}>
+  <img src={props.src} class="card-img-top" alt="{props.src}"/>
+  {/* <div class="card-body">
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div> */}
+  </div>
+  </div>
+    );
+}
