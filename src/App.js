270d40726d66fb70df1e3f793bb0dbe52c660d1a
rishika-07 <rishikakhajuria@outlook.com>
2023-06-20 00:07:40 +0530

    fix:event handeling and state updation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {Navbar} from './components/navbar';
 import {Card} from './components/card';
 import './App.css';
@@ -9,23 +10,30 @@ const photos = [
   'https://picsum.photos/id/1004/200/200', 
   'https://picsum.photos/id/1005/200/200', 
   'https://picsum.photos/id/1006/200/200',
-  'https://picsum.photos/id/1/200/300',
-  'https://picsum.photos/id/6/200/300',
-  'https://picsum.photos/id/11/200/300'
+  // 'https://picsum.photos/id/1/200/300',
+  // 'https://picsum.photos/id/6/200/300',
+  // 'https://picsum.photos/id/11/200/300'
 ]
 function App() {
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapse] = useState(false);
+  const toggle = () => {
+    collapse(!isCollapsed);
+  }
   return (
     <>
-    <div className="landing" style={{color:"blue"}}>
+    
     {/* fragments <> is used when we have to tags together so we need to put up a parent tag */}
     <Navbar/>
     <div className="headding text-center">
+    <button className="btn btn-warning mx-2" onClick={() => setItems([ 'https://picsum.photos/id/1009/200/200',...items])}>+Add</button>
+    <button className="btn btn-success" onClick={toggle}>collapse</button>
     <h1>GALLERY</h1>
      </div>
      <div className="row">
     {photos.map((photo)=><Card src={photo}/>)}
     </div>
-    </div>
+    
    </>
   );
 }
